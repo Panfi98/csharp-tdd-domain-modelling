912e040a58c40427da29281221e13c4d51405c6b
panfi98 <danielpanf@gmail.com>
2024-08-14 14:14:22 +0200

    Finished with exersize two

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    public class Basket
+    {
+        private Dictionary<string, decimal> _items = new Dictionary<string, decimal>();
+        private decimal _total = 0;
+
+
+        public bool Add(string item, decimal price)
+        {
+            if (_items.ContainsKey(item))
+            {
+                
+                return false;
+            }
+            else
+            {
+                _items.Add(item, price);
+                return true;
+            }
+        }
+
+        public decimal Total()
+        {
+            foreach (int value in _items.Values)
+            {
+                _total += value;
+            }
+
+            return _total;
+        }
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        [TestCase("Bread", 0.99,  true)]
+        [TestCase("Kinder Surprise", 0.99, false)]
+        public void TestAdd(string item, decimal price, bool isItemPresent)
+        {
+            //arrange
+            Basket basket = new Basket();
+
+            basket.Add("Apple 100g", 2.49m);
+            basket.Add("Carrot 100g", 1.99m);
+            basket.Add("Kinder Surprise", 0.99m);
+             
+
+            //act
+            bool result1 = basket.Add(item, price);
+
+            //assert
+            Assert.AreEqual(isItemPresent, result1);
+
+        }
+        [TestCase(9)]
+        public void TestTotal(decimal expected)
+        {
+            //arrange
+            Basket basket = new Basket();
+            basket.Add("Apple 100g", 2);
+            basket.Add("Carrot 100g", 3);
+            basket.Add("Kinder Surprise", 4);
+            //act
+            decimal result = basket.Total();
+
+            //assert
+            Assert.IsTrue(result == expected);
+        }
+    }
+}
